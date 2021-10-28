d4164c3f0f027d6ecf00dcb06e16ac06a5d67796
Wagner Santos <prog.wagner@gmail.com>
2021-10-28 02:54:38 -0300

    simple home

--- app.py
+++ app.py
@@ -2,27 +2,20 @@ import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-class Details(BaseModel):
-    f_name: str
-    l_name: str
-    phone_number: int
+class User(BaseModel):
+    user_id: str
 
 
 app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'message': "This is the home page of this API. Go to /apiv1/<name> or /apiv2/?name=<string>"}
+    return {'message': "This is the home page of this API. Go to /docs to see available endpoints"}
 
 
-@app.get('/apiv1/{name}')
-def api1(name: str):
-    return {'message': f'Hello! @{name}'}
-
-
-@app.get('/apiv2/')
-def api2(name: str):
-    return {'message': f'Hello! @{name}'}
+@app.get('/api/{user_id}')
+def api1(user_id: str):
+    return {'message': f'Hello! @{user_id}'}
 
 
 if __name__ == '__main__':
