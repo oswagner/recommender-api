cb293334431de23c370683905b6933913bd4ce87
Wagner Santos <prog.wagner@gmail.com>
2021-10-28 03:43:11 -0300

    add endpoints and gitignore

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
-from pydantic.tools import parse_obj_as
 import uvicorn
+import json
 from collect_data import DataHandler
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-class User(BaseModel):
-    user_id: str
-
-
 app = FastAPI()
-
 data_handler = DataHandler()
 
 @app.get('/')
 def index():
-    return {'message': "This is the home page of this API. Go to /docs to see available endpoints"}
+    return {'message': "This is the home page of this API. Go to /docs or /redoc to see available endpoints"}
 
-
-@app.get('/api/user/{user_id}')
-def sample_request(user_id: str):
-    return {'message': f'Hello! @{user_id}'}
+@app.get('/api/user/{user_name}')
+def sample_request(user_name: str):
+    return {'message': f'Hello! @{user_name}'}
 
 @app.get('/api/most_used')
 def most_used_techniques():
-    most_used_techniques = data_handler.most_used_techniques(as_json=True)
-    print(most_used_techniques)
-    return most_used_techniques
+    most_used_techniques_str = data_handler.most_used_techniques(as_json=True)
+    json_data = json.loads(most_used_techniques_str)
+    return json_data
+
+@app.get('/api/top_rated')
+def top_rated_techniques():
+    most_used_techniques_str = data_handler.top_rated_techniques(as_json=True)
+    json_data = json.loads(most_used_techniques_str)
+    return json_data
 
 
 if __name__ == '__main__':
