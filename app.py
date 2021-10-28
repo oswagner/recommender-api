ce68e225909d8b0c37d56301827505963351b413
Wagner Santos <prog.wagner@gmail.com>
2021-10-28 02:33:48 -0300

    initi

---
+++ app.py
@@ -0,0 +1,29 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Details(BaseModel):
+    f_name: str
+    l_name: str
+    phone_number: int
+
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return {'message': "This is the home page of this API. Go to /apiv1/<name> or /apiv2/?name=<string>"}
+
+
+@app.get('/apiv1/{name}')
+def api1(name: str):
+    return {'message': f'Hello! @{name}'}
+
+
+@app.get('/apiv2/')
+def api2(name: str):
+    return {'message': f'Hello! @{name}'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=4000, debug=True)
