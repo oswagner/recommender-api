8b4ec4e50d6226e2c63842f7b50691efa240de06
Wagner Santos <prog.wagner@gmail.com>
2021-11-15 22:36:01 -0300

    add metadata docs

--- app.py
+++ app.py
@@ -3,7 +3,30 @@ from fastapi import FastAPI
 from routes import api
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+tags_metadata = [
+    {
+        "name": "Personalized by similar users",
+        "description": "Técnicas recomendadas que levam em consideração o usuário solicitante, e/ou usuários similares",
+    },
+    {
+        "name": "Non-personalized",
+        "description": "Técnicas recomendadas que não levam em consideração o usuário solicitante, e/ou usuários similares",
+    },
+]
+
+app = FastAPI(
+    title="HeliusRecommenderAPI",
+    description="Helius Recommender Module API helps you to improve design thinking techniques selection",
+    version="1.0",
+    contact={
+        "name": "Wagner Oliveira dos Santos",
+        "email": "w.santos@edu.pucrs.br",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    openapi_tags=tags_metadata)
 
 app.add_middleware(
     CORSMiddleware,
